4e09757175c2a37aac35c461c4add1d3995ef813
Sergey Levkovich <s.levkovich@sdventures.com>
2023-09-10 21:49:47 +0200

    Revert "add .env"
    This reverts commit 8c7f277097fc0970cb2b5071d6f19a570aa1bd0a.

--- src/services/scores.js
+++ src/services/scores.js
@@ -1,20 +1,23 @@
+const ENDPOINT = 'https://unrivaled-florentine-86a035.netlify.app/.netlify/functions/api'
+// const ENDPOINT = 'http://localhost:9000/.netlify/functions/api'
+
 export async function getScores() {
-    return await fetch(process.env.API + '/scores').then(res => {
+    return await fetch(ENDPOINT + '/scores').then(res => {
         return res.json()
     })
 }
 
-export async function addScore({ username, score, accuracy }) {
-    await fetch(process.env.API + '/add-score', {
+export async function addScore({username, score, accuracy}) {
+    await fetch(ENDPOINT + '/add-score', {
         method: 'POST',
         body: JSON.stringify({
             username,
             score,
-            accuracy,
+            accuracy
         }),
         headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
         },
     })
 }
